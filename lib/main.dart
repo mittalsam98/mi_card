1a2dd34ce8d6ef5ed2771c23a740ffbbd10f7332
Sachin <mittalsam98@gmail.com>
2020-04-02 18:56:48 +0530

    added card widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-              child: Column(
+          home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -20,15 +20,46 @@ void main() => runApp(
                     fontWeight: FontWeight.bold),
               ),
               Text(
-                'Developer',
+                'MERN Developer',
                 style: TextStyle(
                     fontFamily: 'Source Sans Pro',
-                    fontSize: 40,
-                    color: Colors.black,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '9050909461',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source San Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  )),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  color: Colors.white,
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'mittalsam98@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source San Pro',
+                          fontSize: 20.0,
+                        ),
+                      ))),
             ],
-          )),
+          ),
         ),
-      ),
+      )),
     );
